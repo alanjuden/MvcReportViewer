55caa5ffc6f30e966689092e1d67208aefdc05a7
Alan Juden <alanjuden@gmail.com>
2016-11-27 23:18:46 -0600

    #2 - Figure out how to get the project on NuGet.

---
+++ nuget/content/Controllers/ReportController.cs.pp
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace $rootnamespace$.Controllers
+{
+    public class ReportController : AlanJuden.MvcReportViewer.ReportController
+    {
+		protected override ICredentials NetworkCredentials
+		{
+			get
+			{
+				//Custom Domain authentication (be sure to pull the info from a config file)
+				//return new System.Net.NetworkCredential("username", "password", "domain");
+
+				//Default domain credentials (windows authentication)
+				return System.Net.CredentialCache.DefaultNetworkCredentials;
+			}
+		}
+
+		protected override string ReportServerUrl
+		{
+			get
+			{
+				//You don't want to put the full API path here, just the path to the report server's ReportServer directory that it creates (you should be able to access this path from your browser: https://YourReportServerUrl.com/ReportServer/ReportExecution2005.asmx )
+				return "https://YourReportServerUrl.com/ReportServer";
+			}
+		}
+
+		public ActionResult MyReport(string namedParameter1, string namedParameter2)
+		{
+			var model = this.GetReportViewerModel(Request);
+			model.ReportPath = "/Folder/Report File Name";
+			model.AddParameter("Parameter1", namedParameter1);
+			model.AddParameter("Parameter2", namedParameter2);
+
+			return View("ReportViewer", model);
+		}
+	}
+}
